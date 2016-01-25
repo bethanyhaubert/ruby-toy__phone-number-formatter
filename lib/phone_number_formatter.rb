ad6e59a2db043c1d347dc268b88ef01c087e0cfd
Beth Haubert <bethany.haubert@gmail.com>
2016-01-25 13:09:14 -0600

    wip

--- lib/phone_number_formatter.rb
+++ lib/phone_number_formatter.rb
@@ -1,6 +1,15 @@
 # This method takes a string like `"4122226644"` and
 # returns a properly formatted phone number.
+def select_only_integers(phone_number_str)
+	#remove all the non integers from the string
+	phone_number = phone_number_str.gsub(/\D/, '')	
+	return phone_number
+end
+
 
 def format_phone_number(phone_number_str)
-  
+	phone_number = select_only_integers
+	#input the integers in the correct location
+  a = "(" + phone_number(0..2) + ")" + " " + phone_number(3..5) + "-" + phone_number(6..9)
+  return a
 end
